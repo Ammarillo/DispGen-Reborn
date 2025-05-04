2dfd8ed7481a807b8a6b0b3608c9af4005216e9c
Aui <brenker.aurelio@gmx.de>
2025-05-04 16:33:53 +0200

    Cleanup Repo

--- Build.cmd
+++
@@ -1,5 +0,0 @@
-.\DispGen\Scripts\activate
-
-pyinstaller --onefile --windowed --name "DispGen-Reborn" --hidden-import=pyqtgraph.opengl --hidden-import=OpenGL --hidden-import=OpenGL.GL --hidden-import=OpenGL.GLU --icon="resources\D-Gen_Icon.ico" main.py
-
-pyinstaller --clean DispGen-Reborn.spec
